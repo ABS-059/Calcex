fd0fd974e3e907794f1ff55744f3c5ffb438c891
ABS-059 <140780598+ABS-059@users.noreply.github.com>
2023-07-28 06:25:13 -0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+# Operator	Operator Name	Example
+# +	Addition	15+7
+# -	Subtraction	15-7
+# *	Multiplication	5*7
+# **	Exponential	5**3
+# /	Division	5/3
+# %	Modulus	15%7
+# //	Floor Division	15//7
+
+print("*********Calculator********")
+a= 55
+b= 3
+
+
+print("The value of", a,"+", b, "is:", a+b)
+print("The value of", a,"-", b, "is:", a-b)
+print("The value of", a,"*", b, "is:", a*b)
+print("The value of", a,"/", b, "is:", a/b)
+print("The value of", a,"//", b, "is:", a//b)
+print("The value of", a,"**", b, "is:", a**b)
